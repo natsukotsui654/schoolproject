cd281a8c9928e9d32cd481bce198b36055e46d6e
natsukotsui654 <natsukotsui654@gmail.com>
2025-04-06 13:13:39 +0000

    Add 23.go

---
+++ src/23.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Define some variables
+    var a int = 5
+    var b string = "hello world"
+    
+    // Print the values
+    fmt.Println("Value of a:", a)
+    fmt.Println("Value of b:", b)
+}
