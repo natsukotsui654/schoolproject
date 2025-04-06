8616e4abd526ea6ded81219012ef1e7664d3d43f
natsukotsui654 <natsukotsui654@gmail.com>
2025-04-06 13:44:00 +0000

    Add 28.go

---
+++ src/28.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    var x int = 5;
+    if x == 3 {
+        fmt.Println("x is equal to 3")
+    } else {
+        fmt.Println("x is not equal to 3")
+    }
+}
